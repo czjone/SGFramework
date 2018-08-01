9a8ab9adbe9332a8a43d43013bcc7705d0757861
czjone <czjone@126.com>
2018-08-02 01:10:11 +0800

    测试编辑器中设置lua函数的调用
    测试编辑器中设置lua函数的调用

--- Assets/SGFramework/Editor/XLuaExt/LuaButtonEditor.cs
+++ Assets/SGFramework/Editor/XLuaExt/LuaButtonEditor.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[UnityEditor.CustomEditor(typeof(LuaUIButton))]
 public class LuaButtonEditor : UnityEditor.UI.ButtonEditor {
 
-	public string sssss;
+	private LuaUIButton luiBt;
+
+	public override void OnInspectorGUI(){
+		luiBt = target as LuaUIButton;
+		base.DrawDefaultInspector();
+	}
 }
--- Assets/SGFramework/SGFScript/XLuExt/LuaUIButton.cs
+++ Assets/SGFramework/SGFScript/XLuExt/LuaUIButton.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LuaUIButton : UnityEngine.UI.Button {
-    public string LuaActionName { get; set; }
+    public string LuaActionName;
+
+    private void Awake() {
+        this.onClick.AddListener(this.InvorkLuaHandler);
+    }
 
     public void InvorkLuaHandler () {
-
+        SGF.Unity.ULog.D(this.LuaActionName);
     }
 }
